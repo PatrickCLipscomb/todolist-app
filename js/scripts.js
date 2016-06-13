deaca1813b9c5c74342ba2e640d13f25a93d945f
Patrick Lipscomb and Wolfgang Warneke <wolfgang@>
2016-06-13 16:39:58 -0700

    add functionality to add new groups

--- js/scripts.js
+++ js/scripts.js
@@ -3,26 +3,21 @@ var itemToAdd;
 var addDueDate;
 var filterStatus;
 var groupStatus;
+var completedTasks = [];
 
-function toDoItem(name, description, time, group, duedate) {
+function toDoItem(name, description, time, groupkey, duedate) {
   this.itemName = name;
   this.itemDescription = description;
   this.itemEstTime = time;
-  this.itemGroup = group;
+  this.itemGroup = [groupkey];
   this.itemDueDate = duedate;
-  this.completeionStatus = false;
+  this.completetionStatus = false;
   this.dateCreated = [currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()];
-}
-
-function groupList(name, description) {
-  this.groupName = name;
-  this.groupDescription = description;
 }
 
 var allToDoItemsKeys = [];
 var allToDoItems = {};
-var allGroupListsKeys = [];
-var allGroupLists = {};
+var groups = ["Default", "Completed"];
 
 function resetFields() {
   $('#toDoList').empty();
@@ -39,11 +34,20 @@ function moreInfo() {
   });
 }
 
+function markAsComplete() {
+  $('#toDoList li input').click(function() {
+    var key = $(this).prev().prev().prev().text();
+    allToDoItems[key].completetionStatus = true;
+    allToDoItems[key].itemGroup.push("Completed");
+  });
+}
+
 function updateList() {
   allToDoItemsKeys.forEach(function(key) {
     $('#toDoList').append('<li><span class="name">' + allToDoItems[key].itemName + '</span><span class="moreinfo">more info</span><label for="check' + key + '">Complete</label> <input type="checkbox" id="button' + key + '" name="complete" value="" /></li>');
   });
   moreInfo();
+  markAsComplete();
 }
 
 $(document).ready(function() {
@@ -64,11 +68,12 @@ $(document).ready(function() {
 
   $('form#addNewGroup').submit(function(event) {
     event.preventDefault();
-
     var addGroupName = $('input#addGroupName').val();
-    var addGroupDescription = $('input#Group[Description').val();
-    console.log(addGroupName);
+    groups.push(addGroupName);
+    $('#addGroup').append('<option value="' + addGroupName + '">' + addGroupName + '</option>')
   });
+
+
 
 
 });
