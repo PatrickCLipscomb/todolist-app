c23eaa225bb460ea95331130cdbe154fd0e54d15
Patrick Lipscomb and Wolfgang Warneke <patrick+wolfgang@>
2016-06-13 13:35:49 -0700

    finish creating basic layout for all page areas

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,14 @@ function groupList(name, description) {
   this.groupDescription = description;
 }
 
+function allToDoItems(itemobject) {
+  this.itemName = itemobject;
+}
+
+function allGroupLists(groupobject) {
+  this.groupName = nameobject;
+}
+
 $(document).ready(function() {
   $('form#addNewItem').submit(function(event) {
     event.preventDefault();
@@ -22,9 +30,16 @@ $(document).ready(function() {
     var addDescription = $('input#addDescription').val();
     var addEstTime = $('input#addEstTime').val();
     var addGroup = $('input#addGroup').val();
+    console.log(addName);
 
+  });
 
+  $('form#addNewGroup').submit(function(event) {
+    event.preventDefault();
 
-  })
+    var addGroupName = $('input#addGroupName').val();
+    var addGroupDescription = $('input#Group[Description').val();
+    console.log(addGroupName);
+  });
 
-}
+});
